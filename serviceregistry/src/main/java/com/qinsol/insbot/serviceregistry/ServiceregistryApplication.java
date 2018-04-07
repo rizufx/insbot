79dadf921b772b096869c4df1d620828ca97f2d9
Rizwan Mohammed <rizufx@gmail.com>
2018-04-08 01:18:21 +0530

    First Commit

---
+++ serviceregistry/src/main/java/com/qinsol/insbot/serviceregistry/ServiceregistryApplication.java
@@ -0,0 +1,12 @@
+package com.qinsol.insbot.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceregistryApplication.class, args);
+	}
+}
